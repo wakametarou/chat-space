f2fcc5cb96aa0c83aaa0d4b77fb5ec665f3fbdc0
wakametarou <60685052+wakametarou@users.noreply.github.com>
2020-02-12 13:02:26 +0900

    view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  get 'messages/index'
+
+  root "messages#index"  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+>>>>>>> Stashed changes
 end
