a28fb4d64c7ca7e0f79ff5a0aca7cbc38d602790
wakametarou <60685052+wakametarou@users.noreply.github.com>
2020-02-19 15:03:57 +0900

    save

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,34 +40,59 @@ $(function(){
    };
  }
  
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action');
 
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
-  })
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+    })
 
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);      
-    $('form')[0].reset();
-  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);      
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+    $('.messages').animate({
+      scrollTop: $('.messages')[0].scrollHeight
+    });
 
-  .fail(function() {
-    alert("メッセージ送信に失敗しました");
+      return false;
   });
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight})
+        ;}
+      })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 
-  $('.messages').animate({
-     scrollTop: $('.messages')[0].scrollHeight
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+
   });
-
-  return false;
-})
-});
